d9ff5a41c837125cd332dfa62926c9d43740fac2
Matthew Schmitt <matthew.schmitt@riskiq.net>
2020-12-23 14:22:08 -0800

    Removing yumrepo dependancy

--- manifests/mod_fcgid.pp
+++ manifests/mod_fcgid.pp
@@ -2,13 +2,14 @@
 #   ATTENTION!!! If you have EPEL already activated, the yumrepo['mod_epel'] is not necessary.
 #
 class thruk::mod_fcgid {
-  yumrepo  {'mod_epel':
-    baseurl  => 'http://download.fedoraproject.org/pub/epel/6/$basearch',
-    gpgcheck => '0',
-    enabled  => '1',
-  }
+  #yumrepo  {'mod_epel':
+  #  baseurl  => 'http://download.fedoraproject.org/pub/epel/6/$basearch',
+  #  gpgcheck => '0',
+  #  enabled  => '1',
+  #}
 
   package {'mod_fcgid':
-    require =>Yumrepo['mod_epel'],
+    ensure   => present,
+    #require => Yumrepo['mod_epel'],
   }
 }
