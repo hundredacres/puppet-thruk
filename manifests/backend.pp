f07263e7582fa42365c58ce752e3dd214e59da3a
Matthew Schmitt <matthew.schmitt@riskiq.net>
2021-01-26 10:15:48 -0800

    Updating backend to create the peer_resource file with a passed source

--- manifests/backend.pp
+++ manifests/backend.pp
@@ -8,12 +8,21 @@ define thruk::backend (
   String                                            $peer_name,
   Enum['livestatus', 'configonly', 'http', 'mysql'] $peer_type,
   String                                            $peer_path,
-  Optional[Integer]                                 $hidden             = undef,
-  Optional[String]                                  $peer_resource_file = undef,
-  Optional[String]                                  $core_conf          = undef,
-  Optional[String]                                  $obj_check_cmd      = undef,
-  Optional[String]                                  $obj_reload_cmd     = undef,
+  Optional[Integer]                                 $hidden                    = undef,
+  Optional[String]                                  $peer_resource_file        = undef,
+  Optional[String]                                  $peer_resource_file_source = undef,
+  Optional[String]                                  $core_conf                 = undef,
+  Optional[String]                                  $obj_check_cmd             = undef,
+  Optional[String]                                  $obj_reload_cmd            = undef,
 ) {
+  if ($peer_resource_file) {
+    ensure_resource('file', $peer_resource_file, {
+      ensure => present,
+      owner  => apache,
+      group  => apache,
+      source => $peer_resource_file_source,
+    })
+  }
   ensure_resource('file', "/etc/thruk/thruk_local.d/${title}.conf", {
     ensure  => present,
     owner   => apache,
